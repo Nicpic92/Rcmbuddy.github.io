1726c37795db7b7986bdec7895d8edd352a166b5
Nicpic92 <nickolaspicardircm@gmail.com>
2024-09-14 18:32:27 -0400

    remove

--- php/confirmation_email.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = $email;
-$subject = "Registration Confirmation";
-$message = "Hello " . $username . ",\n\nThank you for registering. Your username is " . $username . " and your password is " . $password . ".\n\nBest regards,\nRCMBuddy Team";
-$headers = "From: no-reply@rcmbuddy.com";
-
-mail($to, $subject, $message, $headers);
-?>
